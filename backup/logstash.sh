1208569701b948adb0b4940a29930d398d56556c
Yunsik <55424354+kangyunsik@users.noreply.github.com>
2022-10-04 11:08:30 +0900

    Delete backup directory

--- backup/logstash.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-docker run --name logstash -d -p 5044:5044 \
-  --volume "$PWD/logstash-sample.conf:/usr/share/logstash/config/logstash-sample.conf:ro" \
-  --volume "certs:/etc/logstash/config/certs" \
-  --volume "$PWD/logstash.yml:/usr/share/logstash/config/logstash.yml:ro" \
-  --network elastic \
-  docker.elastic.co/logstash/logstash:8.4.1
